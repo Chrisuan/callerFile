38e0489f7b63263e755e0fc34d66c10700e858d3
Christian <urban_chr@yahoo.de>
2017-03-17 23:20:46 +0100

    debug and release target file

--- callerFile/Program.cs
+++ callerFile/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +9,26 @@ using System.Threading.Tasks;
 namespace callerFile {
     class Program {
         static void Main(string[] args) {
+            
             ProcessStartInfo stinfo = new ProcessStartInfo();
-            stinfo.FileName = @"C:\Users\Christian\Documents\Visual Studio 2017\Projects\getResult\getResult\bin\Debug\getResult.exe";
+            
+            stinfo.FileName = getDebugFile();
             stinfo.UseShellExecute = true;
             stinfo.CreateNoWindow = true;
             Process myProcess = Process.Start(stinfo.FileName, "yes");
             
+        }
+
+        public static string getDebugFile() {
+            var target = @"C:\Users\Christian\Documents\Visual Studio 2017\Projects\getResult\getResult\bin\Debug\getResult.exe";
+            return target;
+        }
+
+
+        public static string getReleaseFile() {
+            string currentDirectory = Environment.CurrentDirectory;
+            string target = currentDirectory + "\\getResult.exe";
+            return target;
         }
     }
 }
