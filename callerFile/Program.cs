2a8afdb5effe3066834be012b98b85d1707073dc
Christian <urban_chr@yahoo.de>
2017-03-17 22:11:10 +0100

    File can be opened

--- callerFile/Program.cs
+++ callerFile/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,7 +8,12 @@ using System.Threading.Tasks;
 namespace callerFile {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hallo");
+            ProcessStartInfo stinfo = new ProcessStartInfo();
+            stinfo.FileName = @"C:\Users\Christian\Documents\Visual Studio 2017\Projects\getResult\getResult\bin\Debug\getResult.exe";
+            stinfo.UseShellExecute = true;
+            stinfo.CreateNoWindow = true;
+            Process myProcess = Process.Start(stinfo.FileName, "yes");
+            
         }
     }
 }
